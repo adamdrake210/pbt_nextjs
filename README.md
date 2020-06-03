11455318748ec2e8e2e1aa5805e0bbffb01b9346
Adam Drake <addrake@ebay.com>
2020-06-03 14:18:16 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Paper Back Travels
+
+A website to gives you introductions and overviews about the best books out
+there for bettering and improving yourself.
---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_API_APP_ID,
+  appId: process.env.FIREBASE_API_APP_ID,
+};
+
+export default firebaseConfig;
