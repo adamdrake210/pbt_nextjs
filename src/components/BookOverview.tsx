d0658693090e2a899e029d2c27b9c57c61f8dae6
Adam Drake <addrake@ebay.com>
2020-06-19 16:52:06 +0200

    book-overviews

--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
-import { useColorMode, Heading, Text, Flex, Stack } from '@chakra-ui/core';
+import React, { useEffect, useState } from 'react';
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Stack,
+  Box,
+  List,
+  ListItem,
+} from '@chakra-ui/core';
+import { frontMatter as bookOverviewPosts } from '../pages/book-overviews/**/*.mdx';
+import Link from 'next/link';
+
+type FrontMatterType = {
+  title: string;
+  author: string;
+  summary: string;
+  slug: string;
+  category: string;
+  publishedDate: string;
+  published: boolean;
+};
 
 export default function BookOverview() {
+  const [categories, setCategories] = useState([]);
+
+  function makeCategoryList(category: string) {
+    return (
+      <Box key={category}>
+        <Heading as="h2">{category}</Heading>
+        <List styleType="disc">
+          {bookOverviewPosts.map((frontMatter: FrontMatterType) => {
+            if (category === frontMatter.category) {
+              return (
+                <Link
+                  key={frontMatter.slug}
+                  passHref
+                  href={`/book-overviews/${frontMatter.category}/${frontMatter.slug}`}
+                >
+                  <a>
+                    <ListItem>{`${frontMatter.title} - ${frontMatter.author}`}</ListItem>
+                  </a>
+                </Link>
+              );
+            }
+          })}
+        </List>
+      </Box>
+    );
+  }
+
+  useEffect(() => {
+    const filterData = bookOverviewPosts.map((frontMatter: FrontMatterType) => {
+      return frontMatter.category;
+    });
+    setCategories(filterData);
+  }, [bookOverviewPosts]);
+
   return (
     <div>
       <Heading as="h1" size="2xl">
         Book Overviews
       </Heading>
+      {categories && (
+        <Box>
+          {categories.map(category => {
+            return makeCategoryList(category);
+          })}
+        </Box>
+      )}
     </div>
   );
 }
