1d101d6c60ca72d25570cd35b5078276b417fbeb
Adam Drake <addrake@ebay.com>
2020-07-12 15:28:56 +0200

    update about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,26 +4,25 @@ import { Heading, Text, Box } from '@chakra-ui/core';
 export default function About() {
   return (
     <Box p={[4, 8]}>
-      <Heading as="h1" size="2xl">
+      <Heading as="h1" size="2xl" mb={4}>
         About Us
       </Heading>
-      <Text>
-        Paperback Travels is a celebration of non-fiction books and their power to educate, inspire and challenge their readers.
-
+      <Text mb={4}>
+        Paperback Travels is a celebration of non-fiction books and their power
+        to educate, inspire and challenge their readers.
       </Text>
-      <Text>
-        We hope you will come back regularly to read summaries of many great books as well as in-depth articles and interviews with interesting people who love reading (almost) as much as we do.
-
-
+      <Text mb={4}>
+        We hope you will come back regularly to read summaries of many great
+        books as well as in-depth articles and interviews with interesting
+        people who love reading (almost) as much as we do.
       </Text>
-       <Text>
-        If you have any feedback about the books and people featured on the site, or if you just want to say hi, we would love to hear from you. Please get in touch via info@paperbacktravels.com. You can also make our day by signing up for our sporadic newsletter using the form below. 
-
+      <Text mb={4}>
+        If you have any feedback about the books and people featured on the
+        site, or if you just want to say hi, we would love to hear from you.
+        Please get in touch via info@paperbacktravels.com. You can also make our
+        day by signing up for our sporadic newsletter using the form below.
       </Text>
-      <Text>
-      Happy reading!
-      </Text>
-      
+      <Text>Happy reading!</Text>
     </Box>
   );
 }
