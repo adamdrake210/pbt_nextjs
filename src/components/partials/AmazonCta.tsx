0f4a9c3f4be1688c6fc8b4d02a13183269e033db
Adam Drake <addrake@ebay.com>
2020-12-03 17:33:56 +0100

    adding interview Jose

--- src/components/partials/AmazonCta.tsx
+++ src/components/partials/AmazonCta.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const AmazonCta: React.FC<Props> = ({ link, bookSummary }) => {
   return (
     <Link href={link} isExternal _hover={{ textDecoration: 'none' }}>
-      {/* <button>
+      {/* <Button colorScheme="blue" size="lg" mt={4}>
         Get this book on{' '}
         <Image
           src={`../${bookSummary ? '../' : ''}amazon-white-logo.png`}
@@ -19,7 +19,7 @@ export const AmazonCta: React.FC<Props> = ({ link, bookSummary }) => {
           ml="7px"
           mt="12px"
         />
-      </button> */}
+      </Button> */}
     </Link>
   );
 };
