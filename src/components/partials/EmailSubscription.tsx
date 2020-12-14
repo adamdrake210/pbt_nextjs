71958388378c81ba43d004e27e17008beed4d053
Adam Drake <addrake@ebay.com>
2020-12-14 16:56:13 +0100

    WIP new email signup

--- src/components/partials/EmailSubscription.tsx
+++ src/components/partials/EmailSubscription.tsx
@@ -15,19 +15,22 @@ import { isValidEmail } from '../../helpers/validators';
 import { ga_event } from '../../helpers/gtag';
 
 export default function EmailSubscription() {
-  const inputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const firstNameInputRef = useRef(null);
   const toast = useToast();
   const [isLoading, setIsLoading] = useState(false);
 
   const subscribe = async (event: any) => {
     event.preventDefault();
-    let emailInput = inputRef.current.value;
+    let emailInput = emailInputRef.current.value;
+    let firstNameInput = firstNameInputRef.current.value;
     if (isValidEmail(emailInput)) {
       setIsLoading(true);
 
       const res = await fetch('/api/subscribe', {
         body: JSON.stringify({
           email: emailInput,
+          firstName: firstNameInput,
         }),
         headers: {
           'Content-Type': 'application/json',
@@ -36,7 +39,6 @@ export default function EmailSubscription() {
       });
 
       const { error } = await res.json();
-      console.log('Error: ', error);
 
       if (error) {
         const realError = JSON.parse(error);
@@ -86,7 +88,7 @@ export default function EmailSubscription() {
 
   return (
     <>
-      <Flex flexDirection="column" alignItems="center" padding={6} my={2}>
+      <Flex flexDirection="column" alignItems="center" padding={1} my={2}>
         {isLoading && (
           <>
             <Spinner
@@ -102,37 +104,58 @@ export default function EmailSubscription() {
         )}
         {!isLoading && (
           <Box
-            border="2px solid"
+            border="3px solid"
             borderColor="cyan.300"
-            bg="cyan.400"
+            bg="gray.100"
             borderRadius={3}
             padding={6}
             my={12}
             w="100%"
-            maxW={480}
+            maxW={400}
+            boxShadow="0 10px 24px 0 rgba(54, 61, 77, 0.15)"
           >
-            <Heading size="md" color="white">
-              Subscribe to our sporadic newsletter
+            <Heading size="lg" color="black">
+              Join the List
             </Heading>
-            <InputGroup size="md" mt={4}>
+            <Text>
+              Join our mailing list to receive occasional emails containing
+              interviews with professionals from all areas. Find out the books
+              they read to become successful.
+            </Text>
+            <InputGroup
+              size="md"
+              mt={4}
+              display="flex"
+              flexDirection={['column']}
+            >
               <Input
-                aria-label="Email Newsletter Subscription"
+                aria-label="Email Newsletter Subscription First Name"
+                placeholder="First Name"
+                focusBorderColor="teal.300"
+                ref={firstNameInputRef}
+                type="text"
+                backgroundColor="#fff"
+                my={2}
+              />
+              <Input
+                aria-label="Email Newsletter Subscription Email"
                 placeholder="example@email.com"
-                focusBorderColor="pink.400"
-                ref={inputRef}
+                focusBorderColor="teal.300"
+                ref={emailInputRef}
                 type="email"
                 backgroundColor="#fff"
+                my={2}
               />
-              <InputRightElement width="6.75rem">
-                <Button
-                  fontWeight="bold"
-                  h="1.75rem"
-                  size="sm"
-                  onClick={subscribe}
-                >
-                  Subscribe
-                </Button>
-              </InputRightElement>
+              <Button
+                fontWeight="bold"
+                size="md"
+                onClick={subscribe}
+                colorScheme="teal"
+                color="white"
+                my={2}
+              >
+                Join the club
+              </Button>
             </InputGroup>
           </Box>
         )}
